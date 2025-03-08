ea4a5d216502ac366160ee70dbc58499708dd6f4
Pradeep Kamble <pradeepkamble1787@gmail.com>
2025-03-08 20:33:07 +0530

    added style to todo app

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,38 +1,54 @@
 import { useState } from "react";
 import TodoListItem from "./TodoListItem";
+import "../css/todoList.css";
 
-export default function TodoList(){
-    
-    const [todos, setTodos] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-    
-    const addTodo = ()=>{
-        const newTodo = {id: Math.random(), text: inputValue};
-        setTodos([...todos, newTodo]);
-        setInputValue('');
-    }
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-    const removeTodo = (id)=>{
-        const todosAferRemovedItem = todos.filter(currentTodo =>currentTodo.id !== id);
-        setTodos(todosAferRemovedItem);
-    }
+  const addTodo = () => {
+    const newTodo = { id: Math.random(), text: inputValue };
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  };
 
-    return(
-        <div className="container">
-            <h2>Todo List Application!</h2>
-
-            <input type="text"
-                   value={inputValue}
-                   onChange={(e)=>{ setInputValue(e.target.value) }}
-                   placeholder="Add Task"
-                    />
-            <button onClick={addTodo} >Add Todo</button>
-
-            <div style={{marginTop:'20px'}}>
-                {todos.map(currentTodo=>(
-                    <TodoListItem key={currentTodo.id} todo={currentTodo} onRemove={removeTodo} />
-                ))}
-            </div>
-        </div>
+  const removeTodo = (id) => {
+    const todosAferRemovedItem = todos.filter(
+      (currentTodo) => currentTodo.id !== id
     );
+    setTodos(todosAferRemovedItem);
+  };
+
+  return (
+    <div className="container">
+      <div className="mx-auto mt-5 p-1 rounded shadow bg title">
+        <h3>To-do List </h3>
+      </div>
+      <div className="mt-4 rounded">
+        <input className="todo-input"
+          type="text"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          placeholder="Add Task"
+        />
+        <button className="btnTodo" type="button" onClick={addTodo}>
+          Add Task
+        </button>
+      </div>
+
+      <div className="mt-5 rounded bg shadow">
+        <ul className="list-group">
+          {todos.map((currentTodo) => (
+            <TodoListItem
+              key={currentTodo.id}
+              todo={currentTodo}
+              onRemove={removeTodo}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,12 +1,16 @@
+function TodoListItem({ todo, onRemove }) {
+  return (
+    // <li className="list-group-item">
+    //     <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+    //     <label>{todo.text}</label>
+    //     <button onClick={()=>{onRemove(todo.id)}} style={{marginLeft:'20px'}}>Remove</button>
+    // </li>
 
-
-function TodoListItem({todo, onRemove}){
-    return(
-        <div>
-            <span>{todo.text}</span>
-            <button onClick={()=>{onRemove(todo.id)}} style={{marginLeft:'20px'}}>Remove</button>
-        </div>
-    )
+    <li className="list-group-item d-flex justify-content-between align-items-left">
+      <span>{todo.text}</span>
+      <img src="/images/x-circle.svg" alt="Bootstrap" onClick={()=>{onRemove(todo.id)}} width="20" height="20" />   
+    </li>
+  );
 }
 
 export default TodoListItem;
